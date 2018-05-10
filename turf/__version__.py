59ce282af0eee874b0ad3c3e56a60c72ff27da9b
Michael Hucka <mhucka@caltech.edu>
2018-05-10 09:06:38 -0700

    Add version info file

---
+++ turf/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   turf version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/turf
+# =============================================================================
+
+__version__     = '0.1.0'
+__title__       = 'turf'
+__description__ = '_TIND.io URL Fixer_ for caltech.tind.io'
+__url__         = 'https://github.com/caltechlibrary/turf'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD/MIT derivative -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
