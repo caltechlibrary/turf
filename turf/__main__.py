696172e31ca1a7529a80ca11fbeb7c3f1a1d3918
Michael Hucka <mhucka@caltech.edu>
2018-05-10 10:36:44 -0700

    Support XLSX output, and also new flag for count

--- turf/__main__.py
+++ turf/__main__.py
@@ -30,7 +30,7 @@ from turf.messages import msg, color
 # Global constants.
 # .............................................................................
 
-_DEFAULT_FETCH_COUNT = 1000
+_DEFAULT_FETCH_COUNT = 100
 '''How many entries to get at one time from caltech.tind.io.'''
 
 
@@ -38,16 +38,26 @@ _DEFAULT_FETCH_COUNT = 1000
 # ......................................................................
 
 @plac.annotations(
-    input    = ('read from given file instead of network', 'option', 'i'),
-    output   = ('output CSV file to write',                'option', 'o'),
-    quiet    = ('do not print any messages while working', 'flag',   'q'),
-    no_color = ('do not color-code terminal output',       'flag',   'C'),
-    version  = ('print version info and exit',             'flag',   'V'),
-    search   = 'search term to use (default: none)',
+    count    = ('how many results to try to get at one time',      'option', 'c'),
+    input    = ('read MARC XML from file instead of doing search', 'option', 'i'),
+    output   = ('write output in the given file',                  'option', 'o'),
+    quiet    = ('do not print any messages while working',         'flag',   'q'),
+    no_color = ('do not color-code terminal output',               'flag',   'C'),
+    version  = ('print version info and exit',                     'flag',   'V'),
+    search   = 'complete search URL (default: none)',
 )
 
-def main(input=None, output=None, quiet=False, no_color=False,
-         version=False, *search):
+def main(count=_DEFAULT_FETCH_COUNT, input=None, output=None,
+         quiet=False, no_color=False, version=False, *search):
+    '''Look for caltech.tind.io records containing URLs and return updated URLs.
+If given a search query, it should be a complete search url as would be typed
+into a web browser.  If given a file, it should be in MARC XML format.
+
+If given an output file, the results will be written to the file.  The format
+of the file will be deduced from the file name extension (.csv or .xlsx); in
+the absence of a file name extension, it will default to XLS format.  If not
+given an output file, the results will only be printed to the terminal.
+'''
 
     # Our defaults are to do things like color the output, which means the
     # command line flags make more sense as negated values (e.g., "nocolor").
@@ -67,10 +77,20 @@ def main(input=None, output=None, quiet=False, no_color=False,
     if input and not input.endswith('.xml'):
         raise SystemExit(color('"{}" does not appear to be an XML file'
                                .format(input), 'error', colorize))
+    if count and not quiet:
+        msg('Will fetch results in batches of {}'.format(count), 'info', colorize)
     if not output and not quiet:
         msg("No output file specified; results won't be saved.", 'warn', colorize)
     elif not quiet:
-        msg('Output will be written to {}'.format(output, 'info', colorize))
+        msg('Output will be written to {}'.format(output), 'info', colorize)
+    if output:
+        name, extension = os.path.splitext(output)
+        if extension and extension.lower() not in ['.csv', '.xlsx']:
+            raise SystemExit(color('"{}" has an unrecognized file extension'.format(output),
+                                   'error', colorize))
+        else:
+            msg('"{}" has no name extension; defaulting to xls'.format(output),
+                'warn', colorize)
 
     # Let's do this thing.
     results = []
@@ -78,17 +98,17 @@ def main(input=None, output=None, quiet=False, no_color=False,
         if os.path.exists(input):
             if not quiet:
                 msg('Reading MARC XML from {}'.format(input), 'info', colorize)
-            results = entries_from_file(input, colorize, quiet)
+            results = entries_from_file(input, count, colorize, quiet)
         elif os.path.exists(os.path.join(os.getcwd(), file)):
             full_path = os.path.join(os.getcwd(), file)
             if not quiet:
                 msg('Reading MARC XML from {}'.format(full_path), 'info', colorize)
-            results = entries_from_file(input, colorize, quiet)
+            results = entries_from_file(input, count, colorize, quiet)
         else:
             raise SystemExit(color('Cannot find file "{}"'.format(input),
                                    'error', colorize))
     else:
-        results = entries_from_search(search[0], colorize, quiet)
+        results = entries_from_search(search[0], count, colorize, quiet)
 
     if not results:
         msg('No results returned.', 'info', colorize)
@@ -97,7 +117,7 @@ def main(input=None, output=None, quiet=False, no_color=False,
     if output:
         if not quiet:
             msg('Writing CSV file {}'.format(output), 'info', colorize)
-        write_csv(output, results)
+        write_results(output, results)
 
     if not quiet:
         msg('Done.', 'info', colorize)
@@ -113,11 +133,31 @@ def print_version():
     print('License: {}'.format(turf.__license__))
 
 
+def write_results(filename, results):
+    name, extension = os.path.splitext(filename)
+    if extension.lower() == '.csv':
+        write_csv(filename, results)
+    else:
+        write_xls(filename, results)
+
+
 def write_csv(filename, results):
     with open(filename, 'w', newline='') as out:
-        out.write('ID,URL\n')
+        out.write('TIND record id,Original URL,Updated URL\n')
         csvwriter = csv.writer(out, delimiter=',')
         csvwriter.writerows(results)
+
+
+def write_xls(filename, results):
+    import openpyxl
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = 'Results'
+    sheet.sheet_properties.tabColor = 'f7ba0b'
+    sheet.append(['TIND record id', 'Original URL', 'Updated URL'])
+    for row in results:
+        sheet.append(row)
+    wb.save(filename = filename)
 
 
 # Main entry point.
